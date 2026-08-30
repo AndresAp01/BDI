c5656c36387bb3b1b1842d93d4c81a4ef8822786
Luis Andres Acuna Perez <andres.acunaperez@protonmail.com>
2026-08-29 18:13:43 -0600

    añado ddatos de conexión

--- Tarea01/app/main.py
+++ Tarea01/app/main.py
@@ -6,3 +6,24 @@ from fastapi.templating import Jinja2Templates
  
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+#
+# ----------------------- datos de conexion
+SERVIDOR = "localhost"
+PUERTO = 1433
+USUARIO = "sa"
+CONTRASENA = "Bd2026Segura!"
+BASE_DATOS = "BDI_Tarea01"
+
+#
+def obtener_conexion():
+    """Abre una conexion nueva a SQL Server. Se cierra despues de cada uso."""
+    return pymssql.connect(
+        server=SERVIDOR,
+        port=PUERTO,
+        user=USUARIO,
+        password=CONTRASENA,
+        database=BASE_DATOS,
+        as_dict=True, #
+    )
+
