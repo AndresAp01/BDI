69e37d32712d01647a93f0cb0d9425074169b2a7
Luis Andres Acuna Perez <andres.acunaperez@protonmail.com>
2026-08-29 18:11:09 -0600

    empiezo a trabajar en el pytthon

--- Tarea01/app/main.py
+++ Tarea01/app/main.py
@@ -1,3 +1,8 @@
 import re
 import pymssql
 from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+ 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
