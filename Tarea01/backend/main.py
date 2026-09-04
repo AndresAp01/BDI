941ae321dd9e71b03558217217a4d98694a64fab
Angela Muñoz <185718644+amondeza@users.noreply.github.com>
2026-09-04 14:19:00 -0600

    Conexión inicial de FastAPI con SQL Server mediante pymssql. / Agrega el endpoint de prueba /conexion.

--- Tarea01/backend/main.py
+++ Tarea01/backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from basedatos import obtener_conexion
 
 app = FastAPI()
 
 @app.get("/")
-
 def inicio():
-    return{"mensaje":"Backend sí sirve"}
+    return {"mensaje": "Backend sí sirve"}
+
+@app.get("/conexion")
+def probar_conexion():
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT DB_NAME()")
+    resultado = cursor.fetchone()
+
+    conexion.close()
+
+    return {
+        "mensaje": "Conexión exitosa",
+        "base_datos": resultado[0]
+    }
